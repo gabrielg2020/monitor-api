76afcf4285c840d52659bc49341f36eb4f786f2e
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-10-21 02:38:23 +0100

    chore: move entities into simpler files
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- internal/entities/hostQueryParams.go
+++
@@ -1,7 +0,0 @@
-package entities
-
-type HostQueryParams struct {
-	ID        int64  `form:"id"`
-	Hostname  string `form:"hostname"`
-	IPAddress string `form:"ip_address"`
-}
--- internal/entities/metric.go
+++ internal/entities/metric.go
@@ -1,5 +1,17 @@
 package entities
 
+type MetricLatestQueryParams struct {
+	HostID *int64 `form:"host_id"`
+}
+
+type MetricQueryParams struct {
+	HostID    *int64 `form:"host_id"`
+	StartTime *int64 `form:"start_time"`
+	EndTime   *int64 `form:"end_time"`
+	Limit     int    `form:"limit"`
+	Order     string `form:"order"` // "asc" or "desc"
+}
+
 type SystemMetric struct {
 	ID                   int64   `json:"id" db:"id"`
 	HostID               int64   `json:"host_id" db:"host_id"`
